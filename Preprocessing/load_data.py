733723c10caa1db927cfbc7e97bd1098a40c3085
Honglin Bao <baohongl@msu.edu>
2021-07-17 18:40:28 -0400

    Create load_data.py

---
+++ Preprocessing/load_data.py
@@ -0,0 +1,34 @@
+def load_dataset(data_path, seed=123):
+
+    data_path = os.path.join(data_path, 'data')
+
+    # Load the training data
+    train_texts = []
+    train_labels = []
+    for category in ['R', 'D']:
+        train_path = os.path.join(data_path, 'train', category)
+        for fname in sorted(os.listdir(train_path)):
+            if fname.endswith('.txt'):
+                with open(os.path.join(train_path, fname)) as f:
+                    train_texts.append(f.read())
+                train_labels.append(0 if category == 'D' else 1)
+
+    # Load the validation data.
+    test_texts = []
+    test_labels = []
+    for category in ['R', 'D']:
+        test_path = os.path.join(data_path, 'test', category)
+        for fname in sorted(os.listdir(test_path)):
+            if fname.endswith('.txt'):
+                with open(os.path.join(test_path, fname)) as f:
+                    test_texts.append(f.read())
+                test_labels.append(0 if category == 'D' else 1)
+
+    # Shuffle the training data and labels.
+    random.seed(seed)
+    random.shuffle(train_texts)
+    random.seed(seed)
+    random.shuffle(train_labels)
+
+    return ((train_texts, np.array(train_labels)),
+            (test_texts, np.array(test_labels)))
