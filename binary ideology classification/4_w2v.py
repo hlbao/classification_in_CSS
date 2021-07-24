f33e1b6f70bf4a718ffca71f3efecbdbaa0888d0
Honglin Bao <baohongl@msu.edu>
2021-07-24 11:38:55 -0400

    Update 4_w2v.py

--- binary ideology classification/4_w2v.py
+++ binary ideology classification/4_w2v.py
@@ -1,1 +1,149 @@
 #word to vector
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#for text pre-processing
+import re, string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
+#for model-building
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+
+# bag of words
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+#for word embedding
+import gensim
+from gensim.models import Word2Vec #Word2Vec is mostly used for huge datasets
+#you can download the data from https://www.kaggle.com/c/nlp-getting-started/data
+import os
+os.chdir('/Users/honglinbao/Desktop/')
+df_train=pd.read_csv('preprocessed.csv')
+# create Word2vec model
+#here words_f should be a list containing words from each document. say 1st row of the list is words from the 1st document/sentence
+#length of words_f is number of documents/sentences in your dataset
+df_train['clean_text_tok']=[for i in df_train['text']] #convert preprocessed sentence to tokenized sentence
+model = Word2Vec(df_train['clean_text_tok'],min_count=1)  #min_count=1 means word should be present at least across all documents,
+#if min_count=2 means if the word is present less than 2 times across all the documents then we shouldn't consider it
+
+
+w2v = dict(zip(model.wv.index2word, model.wv.syn0))  #combination of word and its vector
+
+#for converting sentence to vectors/numbers from word vectors result by Word2Vec
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        # if a text is empty we should return a vector of zeros
+        # with the same dimensionality as all the other vectors
+        self.dim = len(next(iter(word2vec.values())))
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        return np.array([
+            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+      
+      
+#SPLITTING THE TRAINING DATASET INTO TRAINING AND VALIDATION
+ 
+# Input: "reviewText", "rating" and "time"
+# Target: "log_votes"
+X_train, X_val, y_train, y_val = train_test_split(df_train["clean_text"],
+                                                  df_train["target"],
+                                                  test_size=0.2,
+                                                  shuffle=True)
+X_train_tok= [nltk.word_tokenize(i) for i in X_train]  #for word2vec
+X_val_tok= [nltk.word_tokenize(i) for i in X_val]      #for word2vec
+
+#TF-IDF
+# Convert x_train to vector since model can only run on numbers and not words- Fit and transform
+tfidf_vectorizer = TfidfVectorizer(use_idf=True)
+X_train_vectors_tfidf = tfidf_vectorizer.fit_transform(X_train) #tfidf runs on non-tokenized sentences unlike word2vec
+# Only transform x_test (not fit and transform)
+X_val_vectors_tfidf = tfidf_vectorizer.transform(X_val) #Don't fit() your TfidfVectorizer to your test data: it will 
+#change the word-indexes & weights to match test data. Rather, fit on the training data, then use the same train-data-
+#fit model on the test data, to reflect the fact you're analyzing the test data only based on what was learned without 
+#it, and the have compatible
+
+
+#Word2vec
+# Fit and transform
+modelw = MeanEmbeddingVectorizer(w2v)
+X_train_vectors_w2v = modelw.transform(X_train_tok)
+X_val_vectors_w2v = modelw.transform(X_val_tok)
+
+#FITTING THE CLASSIFICATION MODEL using Logistic Regression(tf-idf)
+
+lr_tfidf=LogisticRegression(solver = 'liblinear', C=10, penalty = 'l2')
+lr_tfidf.fit(X_train_vectors_tfidf, y_train)  #model
+
+#Predict y value for test dataset
+y_predict = lr_tfidf.predict(X_val_vectors_tfidf)
+y_prob = lr_tfidf.predict_proba(X_val_vectors_tfidf)[:,1]
+ 
+
+print(classification_report(y_val,y_predict))
+print('Confusion Matrix:',confusion_matrix(y_val, y_predict))
+ 
+fpr, tpr, thresholds = roc_curve(y_val, y_prob)
+roc_auc = auc(fpr, tpr)
+print('AUC:', roc_auc)
+
+#FITTING THE CLASSIFICATION MODEL using Naive Bayes(tf-idf)
+#It's a probabilistic classifier that makes use of Bayes' Theorem, a rule that uses probability to make predictions based on prior knowledge of conditions that might be related. This algorithm is the most suitable for such large dataset as it considers each feature independently, calculates the probability of each category, and then predicts the category with the highest probability.
+
+nb_tfidf = MultinomialNB()
+nb_tfidf.fit(X_train_vectors_tfidf, y_train)  #model
+
+#Predict y value for test dataset
+y_predict = nb_tfidf.predict(X_val_vectors_tfidf)
+y_prob = nb_tfidf.predict_proba(X_val_vectors_tfidf)[:,1]
+ 
+
+print(classification_report(y_val,y_predict))
+print('Confusion Matrix:',confusion_matrix(y_val, y_predict))
+ 
+fpr, tpr, thresholds = roc_curve(y_val, y_prob)
+roc_auc = auc(fpr, tpr)
+print('AUC:', roc_auc)
+
+#FITTING THE CLASSIFICATION MODEL using Logistic Regression (W2v)
+lr_w2v=LogisticRegression(solver = 'liblinear', C=10, penalty = 'l2')
+lr_w2v.fit(X_train_vectors_w2v, y_train)  #model
+
+#Predict y value for test dataset
+y_predict = lr_w2v.predict(X_val_vectors_w2v)
+y_prob = lr_w2v.predict_proba(X_val_vectors_w2v)[:,1]
+ 
+
+print(classification_report(y_val,y_predict))
+print('Confusion Matrix:',confusion_matrix(y_val, y_predict))
+ 
+fpr, tpr, thresholds = roc_curve(y_val, y_prob)
+roc_auc = auc(fpr, tpr)
+print('AUC:', roc_auc)
+
+#TESTING THE MODEL ON UNLABELLED DATASET
