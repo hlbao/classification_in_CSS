41965f19267bdbf30e603a61143de49bbcd21acf
Honglin Bao <baohongl@msu.edu>
2021-07-24 11:56:32 -0400

    Update 4_w2v.py

--- binary ideology classification/4_w2v.py
+++ binary ideology classification/4_w2v.py
@@ -34,26 +34,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 #for word embedding
 import gensim
 from gensim.models import Word2Vec #Word2Vec is mostly used for huge datasets
-#you can download the data from https://www.kaggle.com/c/nlp-getting-started/data
+#you can download the data from https://www.dropbox.com/s/o715p3dsr55cxhc/preprocessed.csv?dl=0
 import os
 os.chdir('/Users/honglinbao/Desktop/')
+#your own path
+
 df_train=pd.read_csv('preprocessed.csv')
 # create Word2vec model
-#here words_f should be a list containing words from each document. say 1st row of the list is words from the 1st document/sentence
-#length of words_f is number of documents/sentences in your dataset
-df_train['clean_text_tok']=[for i in df_train['text']] #convert preprocessed sentence to tokenized sentence
-model = Word2Vec(df_train['clean_text_tok'],min_count=1)  #min_count=1 means word should be present at least across all documents,
-#if min_count=2 means if the word is present less than 2 times across all the documents then we shouldn't consider it
 
-
+df_train['clean_text_tok']=[nltk.word_tokenize(i) for i in df_train['text']] 
+model = Word2Vec(df_train['clean_text_tok'],min_count=1) 
 w2v = dict(zip(model.wv.index2word, model.wv.syn0))  #combination of word and its vector
 
-#for converting sentence to vectors/numbers from word vectors result by Word2Vec
 class MeanEmbeddingVectorizer(object):
     def __init__(self, word2vec):
         self.word2vec = word2vec
-        # if a text is empty we should return a vector of zeros
-        # with the same dimensionality as all the other vectors
         self.dim = len(next(iter(word2vec.values())))
 
     def fit(self, X, y):
@@ -68,28 +63,19 @@ class MeanEmbeddingVectorizer(object):
       
       
 #SPLITTING THE TRAINING DATASET INTO TRAINING AND VALIDATION
- 
-# Input: "reviewText", "rating" and "time"
-# Target: "log_votes"
-X_train, X_val, y_train, y_val = train_test_split(df_train["clean_text"],
-                                                  df_train["target"],
-                                                  test_size=0.2,
+
+X_train, X_val, y_train, y_val = train_test_split(df_train["text"],
+                                                  df_train["party"],
+                                                  test_size=0.3,
                                                   shuffle=True)
+
 X_train_tok= [nltk.word_tokenize(i) for i in X_train]  #for word2vec
 X_val_tok= [nltk.word_tokenize(i) for i in X_val]      #for word2vec
 
-#TF-IDF
-# Convert x_train to vector since model can only run on numbers and not words- Fit and transform
 tfidf_vectorizer = TfidfVectorizer(use_idf=True)
-X_train_vectors_tfidf = tfidf_vectorizer.fit_transform(X_train) #tfidf runs on non-tokenized sentences unlike word2vec
-# Only transform x_test (not fit and transform)
-X_val_vectors_tfidf = tfidf_vectorizer.transform(X_val) #Don't fit() your TfidfVectorizer to your test data: it will 
-#change the word-indexes & weights to match test data. Rather, fit on the training data, then use the same train-data-
-#fit model on the test data, to reflect the fact you're analyzing the test data only based on what was learned without 
-#it, and the have compatible
+X_train_vectors_tfidf = tfidf_vectorizer.fit_transform(X_train) 
+X_val_vectors_tfidf = tfidf_vectorizer.transform(X_val) 
 
-
-#Word2vec
 # Fit and transform
 modelw = MeanEmbeddingVectorizer(w2v)
 X_train_vectors_w2v = modelw.transform(X_train_tok)
