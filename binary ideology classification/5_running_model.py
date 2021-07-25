5af78a975db9f43b7d5a4a5a88d33e06ff5126f7
Honglin Bao <baohongl@msu.edu>
2021-07-25 11:07:26 -0400

    Update 5_running_model.py

--- binary ideology classification/5_running_model.py
+++ binary ideology classification/5_running_model.py
@@ -2,54 +2,56 @@
 #as step 4 is a word-to-vector process. Step 5 is running different binary classification models.
 
 #FITTING THE CLASSIFICATION MODEL using Logistic Regression(tf-idf)
-
-lr_tfidf=LogisticRegression(solver = 'liblinear', C=10, penalty = 'l2')
+lr_tfidf=LogisticRegression(solver = 'liblinear', C=1.0, penalty = 'l2')
+#lr_tfidf=LogisticRegression(solver = 'liblinear', C=10, penalty = 'l2')
 lr_tfidf.fit(X_train_vectors_tfidf, y_train)  #model
-
-#Predict y value for test dataset
 y_predict = lr_tfidf.predict(X_val_vectors_tfidf)
 y_prob = lr_tfidf.predict_proba(X_val_vectors_tfidf)[:,1]
- 
-
 print(classification_report(y_val,y_predict))
 print('Confusion Matrix:',confusion_matrix(y_val, y_predict))
- 
 fpr, tpr, thresholds = roc_curve(y_val, y_prob)
 roc_auc = auc(fpr, tpr)
 print('AUC:', roc_auc)
 
 #FITTING THE CLASSIFICATION MODEL using Naive Bayes(tf-idf)
-#It's a probabilistic classifier that makes use of Bayes' Theorem, a rule that uses probability to make predictions based on prior knowledge of conditions that might be related. This algorithm is the most suitable for such large dataset as it considers each feature independently, calculates the probability of each category, and then predicts the category with the highest probability.
-
+#the best
 nb_tfidf = MultinomialNB()
 nb_tfidf.fit(X_train_vectors_tfidf, y_train)  #model
-
-#Predict y value for test dataset
 y_predict = nb_tfidf.predict(X_val_vectors_tfidf)
 y_prob = nb_tfidf.predict_proba(X_val_vectors_tfidf)[:,1]
- 
-
 print(classification_report(y_val,y_predict))
 print('Confusion Matrix:',confusion_matrix(y_val, y_predict))
- 
 fpr, tpr, thresholds = roc_curve(y_val, y_prob)
 roc_auc = auc(fpr, tpr)
 print('AUC:', roc_auc)
 
-#FITTING THE CLASSIFICATION MODEL using Logistic Regression (W2v)
-lr_w2v=LogisticRegression(solver = 'liblinear', C=10, penalty = 'l2')
+#FITTING THE CLASSIFICATION MODEL using Logistic Regression (w2v)
+lr_w2v=LogisticRegression(solver = 'liblinear', C=1.0, penalty = 'l2')
 lr_w2v.fit(X_train_vectors_w2v, y_train)  #model
-
-#Predict y value for test dataset
 y_predict = lr_w2v.predict(X_val_vectors_w2v)
 y_prob = lr_w2v.predict_proba(X_val_vectors_w2v)[:,1]
- 
-
 print(classification_report(y_val,y_predict))
 print('Confusion Matrix:',confusion_matrix(y_val, y_predict))
- 
 fpr, tpr, thresholds = roc_curve(y_val, y_prob)
 roc_auc = auc(fpr, tpr)
 print('AUC:', roc_auc)
 
-#TESTING THE MODEL ON UNLABELLED DATASET
+#FITTING THE CLASSIFICATION MODEL using Naive Bayes(w2v)
+nb_w2v = MultinomialNB()
+nb_w2v.fit(X_train_vectors_w2v, y_train)  #model
+y_predict = nb_w2v.predict(X_val_vectors_w2v)
+y_prob = nb_w2v.predict_proba(X_val_vectors_w2v)[:,1]
+print(classification_report(y_val,y_predict))
+print('Confusion Matrix:',confusion_matrix(y_val, y_predict))
+fpr, tpr, thresholds = roc_curve(y_val, y_prob)
+roc_auc = auc(fpr, tpr)
+print('AUC:', roc_auc)
+
+
+# what should be chosen as the evaluation matrix?
+#walk through this page: https://scikit-learn.org/stable/modules/classes.html#sklearn-metrics-metrics
+
+
+
+#add other classification models further
+
