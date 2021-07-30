89c352fa493d0b9390252e814f71c0a3642a98fb
Honglin Bao <baohongl@msu.edu>
2021-07-30 14:15:59 -0400

    Update 1_preprocessing.py

--- multiclass/1_preprocessing.py
+++ multiclass/1_preprocessing.py
@@ -52,7 +52,84 @@ from google.colab import files
 uploaded = files.upload()
 
 train_df = pd.read_csv('train.csv')
-print('Number of data: ', train_df.shape[0])
-print('Number of features: ', train_df.shape[1])
-print('Features: ', train_df.columns.values)
-train_df.head(10)
+#print('Number of data: ', train_df.shape[0])
+#print('Number of features: ', train_df.shape[1])
+#print('Features: ', train_df.columns.values)
+#train_df.head(10)
+
+def cleanHtml(sentence):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, ' ', str(sentence))
+    return cleantext
+
+def cleanPunc(sentence): #function to clean the word of any punctuation or special characters
+    cleaned = re.sub(r'[?|!|\'|"|#]',r'',sentence)
+    cleaned = re.sub(r'[.|,|)|(|\|/]',r' ',cleaned)
+    cleaned = cleaned.strip()
+    cleaned = cleaned.replace("\n"," ")
+    return cleaned
+
+def keepAlpha(sentence):
+    alpha_sent = ""
+    for word in sentence.split():
+        alpha_word = re.sub('[^a-z A-Z]+', ' ', word)
+        alpha_sent += alpha_word
+        alpha_sent += " "
+    alpha_sent = alpha_sent.strip()
+    return alpha_sent
+
+train_df['comment_text'] = train_df['comment_text'].str.lower()
+train_df['comment_text'] = train_df['comment_text'].apply(cleanHtml)
+train_df['comment_text'] = train_df['comment_text'].apply(cleanPunc)
+train_df['comment_text'] = train_df['comment_text'].apply(keepAlpha)
+train_df.head()
+
+#stop_words = set(stopwords.words('english'))
+stop_words= set(['br', 'the', 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",\
+            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', \
+            'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',\
+            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', \
+            'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', \
+            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', \
+            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',\
+            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',\
+            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',\
+            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', \
+            's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', \
+            've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',\
+            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',\
+            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", \
+            'won', "won't", 'wouldn', "wouldn't", 'zero','one','two','three','four','five','six','seven','eight','nine','ten','may','also','across','among','beside','however','yet','within'])
+
+re_stop_words = re.compile(r"\b(" + "|".join(stop_words) + ")\\W", re.I)
+def removeStopWords(sentence):
+    global re_stop_words
+    return re_stop_words.sub(" ", sentence)
+
+train_df['comment_text'] = train_df['comment_text'].apply(removeStopWords)
+train_df.head()
+
+stemmer = SnowballStemmer("english")
+def stemming(sentence):
+    stemSentence = ""
+    for word in sentence.split():
+        stem = stemmer.stem(word)
+        stemSentence += stem
+        stemSentence += " "
+    stemSentence = stemSentence.strip()
+    return stemSentence
+    
+def decontracted(phrase):
+# specific
+    phrase = re.sub(r"won't", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
