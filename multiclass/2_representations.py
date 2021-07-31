2674371e4b08aa757c61ea594738a15751bc0a7c
Honglin Bao <baohongl@msu.edu>
2021-07-31 14:38:19 -0400

    Update 2_representations.py

--- multiclass/2_representations.py
+++ multiclass/2_representations.py
@@ -21,3 +21,17 @@ text_col = ['comment_text']
 drop_col = ['id', 'clean','count_sent', 'count_word', 'count_unique_word', 'word_unique_percent']
 label_col = [col for col in train_df.columns if col not in text_col + drop_col]
 final_tf_idf = tf_idf_vect.transform(preprocessed_comments)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import scipy
+#split
+features = text_col
+X_test = test_df[features].copy()
+X_train, X_val, y_train, y_val = train_test_split(train_df[features], train_df[label_col], test_size=0.2, random_state=2021)
+X_train = tf_idf_vect.transform(X_train['comment_text'])
+X_val = tf_idf_vect.transform(X_val['comment_text'])
+X_test = tf_idf_vect.transform(X_test['comment_text'])
+feature_names = tf_idf_vect.get_feature_names()
+
+print(X_train.shape, y_train.shape, X_val.shape, y_val.shape)
