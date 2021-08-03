cedb4e395f5de1a0873c5f2afba9d7860fdaefbc
Honglin Bao <baohongl@msu.edu>
2021-08-03 14:23:29 -0400

    Update 4_deep_network.py

--- multiclass/4_deep_network.py
+++ multiclass/4_deep_network.py
@@ -1,3 +1,30 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras.layers import Dropout
+import re
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+STOPWORDS = set(stopwords.words('english'))
+from bs4 import BeautifulSoup
+import plotly.graph_objs as go
+import plotly.plotly as py
+import cufflinks
+from IPython.core.interactiveshell import InteractiveShell
+import plotly.figure_factory as ff
+InteractiveShell.ast_node_interactivity = 'all'
+from plotly.offline import iplot
+cufflinks.go_offline()
+cufflinks.set_config_file(world_readable=True, theme='pearl')
+
 #run deep learning
 #should be attached to 2_representations.py
 #I separate them for clear illustration reasons.
