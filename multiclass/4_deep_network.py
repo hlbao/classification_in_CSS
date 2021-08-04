7b08f2ebce397bd4dd9fcbda2337edf9d9dd6f3e
Honglin Bao <baohongl@msu.edu>
2021-08-04 16:08:53 -0400

    Update 4_deep_network.py

--- multiclass/4_deep_network.py
+++ multiclass/4_deep_network.py
@@ -38,18 +38,31 @@ EMBEDDING_DIM = 100
 X_train, X_val, y_train, y_val = train_test_split(train_df[features], train_df[label_col], test_size=0.2, random_state=2021)
 X_train = tf_idf_vect.transform(X_train['comment_text'])
 X_val = tf_idf_vect.transform(X_val['comment_text'])
-#X_test = tf_idf_vect.transform(X_test['comment_text'])
+X_test = tf_idf_vect.transform(X_test['comment_text'])
 feature_names = tf_idf_vect.get_feature_names()
 
+from google.colab import files
+uploaded = files.upload()
+sub_df_mnb = pd.read_csv('sample_submission.csv')
+
 model = Sequential()
-model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=train_df[features].shape[1]))
-model.add(SpatialDropout1D(0.2))
-model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+#model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=train_df[features].shape[1]))
+model.add(Dense(4, activation='relu', input_dim = X_train.shape[1]))
+model.add(Dropout(0.3))
+#model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(6, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 #print(model.summary())
-epochs = 5
-batch_size = 64
+epochs = 10
+batch_size = 32
 
-lstm_model = model.fit(X_train, y_train[label_name], epochs=epochs, batch_size=batch_size,validation_split=0.0,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+#you might need this, depends which version of tenserflow you are working with
+#converting your labels to arrays before calling model.fit()
+X_train = np.asarray(X_train).astype('float32')
+y_train = np.asarray(y_train).astype('float32')
+X_val = np.asarray(X_val).astype('float32')
+y_val = np.asarray(y_val).astype('float32')
+
+lstm_model = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+#lstm_model = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,validation_split=0.0,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
 accr = model.evaluate(X_val,y_val)
