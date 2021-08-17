2b21c884cf5eb5a8844a44c6ce3ee54fb991c12e
Honglin Bao <baohongl@msu.edu>
2021-08-17 15:56:38 -0400

    Create api_tool.py

---
+++ smart_scraper/twitterAPI/api_tool.py
@@ -0,0 +1,2 @@
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAHnOSAEAAAAAWKmBmaqipWHUCz%2BiZMIoRO%2FG1ts%3DH3ymJKodPukYp4NO6ekLqAB4NBVWoVVydMpQ3LNDgZoKg7Mrs4"
+tweet_fields = ["text", "public_metrics", "created_at", "author_id", "geo"]
